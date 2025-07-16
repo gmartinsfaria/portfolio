15c287922dc321d81df7c877aa45d81c428ba932
gmartinsfaria <g.martinsfaria@ua.pt>
2025-07-16 18:19:59 +0100

    Adicionado site RPA no showcase

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const d = new Date();
+    let year = d.getFullYear();
+    console.log("Estamos no ano " + year);
+    document.getElementById("currentYear").innerHTML = year;
+});
